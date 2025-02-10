588718c32e9b0c12713fd2067e547b097c9441fa
tarikku_desu <amehritarik@gmail.com>
2025-02-10 17:23:45 +0100

    source code

---
+++ exam_02/1/ft_strcspn/ft_strcspn.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+// include for size_t
+// #include <stdlib.h>
+
+size_t  ft_strcspn(const char *s, const char *reject)
+{
+	size_t	i;
+	size_t	k;
+
+	i = 0;
+	k = 0;
+	while (s[i] != '\0')
+	{
+		while (reject[k] != '\0')
+		{
+			if (reject[k] == s[i])
+				return (i);
+			k++;
+		}
+		k = 0;
+		i++;
+	}
+	return (i);
+}
+
+// int	main(void)
+// {
+// 	printf("%lu\n", ft_strcspn("test", "es"));
+// 	printf("%lu\n", strcspn("test", "es"));
+// printf("%lu\n", ft_strcspn("test", "f"));
+// 	printf("%lu\n", strcspn("test", "f"));
+// }
